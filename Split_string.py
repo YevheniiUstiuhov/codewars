a8a5ee790a7773e94bfb3f78065e1bd85d4b6af9
YevheniiUstiuhov <123666467+YevheniiUstiuhov@users.noreply.github.com>
2023-02-25 13:54:07 +0200

    Update Split_string.py

--- Split_string.py
+++ Split_string.py
@@ -1,2 +1,9 @@
-for i in range(len(s)):
+def solution(s):
+    a = []
+    for i in range(0,len(s),2):
+        if i!=len(s)-1:
+            a.append(s[i]+s[i+1])
+        else:
+            a.append(s[i]+'_');
+    return a
   
